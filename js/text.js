63dce972054b6494bbb0154caff478f7084e47ea
n-westman <niklas@Niklass-MacBook-Pro.local>
2019-10-20 14:25:58 +0200

    Adding all files to git first time

---
+++ js/text.js
@@ -0,0 +1,77 @@
+var array = [
+    
+    ['Serenity',
+    'My latest work has been a longer project where every detail has been important for the final result of the composition. Through several iterations I have taken a step closer to something I could feel sounded professionally. Which was also the purpose of the song. That it would result in something professional and emotional.'
+    ],
+
+    ['Outlaws',
+    'When the song Hold Me Tight turned out to be a good step forward for my music, I naturally wanted to continue. But Hold Me Tight was not a song I felt myself could identify with. Therefore, I took all the knowledge with me when I instead started trying to find myself in the sound and less focus on what the listeners wanted. '
+    ],
+
+    ['Hold Me Tight',
+    'My music genre has never been carved in stone. I have been around several genres and tried to find my own identity sound. During this period I wanted something to happen, I wanted to get bigger promoters on Youtube to take note and see that I could actually do something good. From there came this powerful song with a strong start and emotional loop, I did my best to give the song what it deserved.'
+    ],
+
+    ['Phenomena',
+    'The inspiration behind phenomena was the image of an african tribe dansing and playing music around a fire. The core of it all was the pure joy they shared with eachother. Without the need to buy things that give them joy.'
+    ],
+
+    ['Reminiscence',
+    'Creating Reminiscence was an experiment in itself. Testing new genres and mixing with vocals could work. The result is an emotion drum and bass track full of ambient effects and emotional vocals.'
+    ],
+    
+    ['Reverberation',
+    'When creating this piece I was totally lost in instruments playing well in the genre of Ambience. What I found was that total control not always results in the best outcome. When letting go of scructures you find new ways that gives something different.'
+    ],
+
+];
+
+//If needed for loop can start here
+
+var para = document.createElement("h2");
+para.innerHTML = array[0][0];
+document.getElementById('firstOne').appendChild(para); 
+
+var para = document.createElement("p");
+para.innerHTML = array[0][1];
+document.getElementById('firstTwo').appendChild(para); 
+
+var para = document.createElement("h2");
+para.innerHTML = array[1][0];
+document.getElementById('secondOne').appendChild(para); 
+
+var para = document.createElement("p");
+para.innerHTML = array[1][1];
+document.getElementById('secondTwo').appendChild(para);
+
+var para = document.createElement("h2");
+para.innerHTML = array[2][0];
+document.getElementById('thirdOne').appendChild(para); 
+
+var para = document.createElement("p");
+para.innerHTML = array[2][1];
+document.getElementById('thirdTwo').appendChild(para);
+
+var para = document.createElement("h2");
+para.innerHTML = array[3][0];
+document.getElementById('fourOne').appendChild(para); 
+
+var para = document.createElement("p");
+para.innerHTML = array[3][1];
+document.getElementById('fourTwo').appendChild(para);
+
+var para = document.createElement("h2");
+para.innerHTML = array[4][0];
+document.getElementById('fiveOne').appendChild(para); 
+
+var para = document.createElement("p");
+para.innerHTML = array[4][1];
+document.getElementById('fiveTwo').appendChild(para);
+
+var para = document.createElement("h2");
+para.innerHTML = array[5][0];
+document.getElementById('sixOne').appendChild(para); 
+
+var para = document.createElement("p");
+para.innerHTML = array[5][1];
+document.getElementById('sixTwo').appendChild(para);
